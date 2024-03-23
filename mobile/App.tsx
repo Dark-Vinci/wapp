c703ab40cfa7b6883383be929fa219cc7e8d9d98
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-03-23 05:55:15 +0000

    chore: more on mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { Text, SafeAreaView } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to linkedin clone</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
