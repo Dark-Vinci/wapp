21e5843ed3c0d70ebf4805a9cb8b892b8a8797ff
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-03-26 03:00:54 +0000

    chorew: more update

---
+++ mobile/src/Components/If/If.tsx
@@ -0,0 +1,14 @@
+import { JSX, ReactNode } from 'react';
+
+interface IfProps {
+  readonly children: ReactNode;
+  readonly condition: boolean;
+}
+
+export function If({children, condition}: IfProps): JSX.Element {
+  if (condition) {
+    return children as JSX.Element
+  }
+
+  return null as unknown as JSX.Element
+}
--- mobile/src/Components/index.ts
+++ mobile/src/Components/index.ts
@@ -2,3 +2,4 @@ export * from './Screen';
 export * from './ErrorBoundary';
 export * from './IF_ELSE';
 export * from './FallBack';
+export * from './If';
